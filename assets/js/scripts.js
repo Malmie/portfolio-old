70dbf550ccc9b9b6e90f793b55f39b0d11bd1757
Malmie <miechurska.m88@gmail.com>
2018-01-28 15:17:22 +0100

    new project

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+$(function(){ 
+//Scroller
+	$(".section-scroll").sectionScroller({
+		scrollerButton: "#section-scroller-button",
+		scrollType: "swing",
+		scrollDuration: 900,
+	});
+
+//text animation
+	function changeText(cont1,cont2,speed){
+		var Otext=cont1.text();
+		var Ocontent=Otext.split("");
+		var i=0;
+		function show(){
+		if(i<Ocontent.length)
+		{  
+  			cont2.append(Ocontent[i]);
+  			i=i+1;
+		};
+		};
+		var Otimer=setInterval(show,speed);
+	};
+  	changeText($("h2"),$(".text-animation"),90); 
+  	clearInterval(Otimer);
+});
